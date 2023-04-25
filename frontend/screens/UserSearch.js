e42bfed62efdfb50003ec575c7cd3459dbd2e7cf
Ted <tadas2410@gmail.com>
2023-04-25 21:46:52 +0100

    fix keyboard persist bug

--- frontend/screens/UserSearch.js
+++ frontend/screens/UserSearch.js
@@ -80,6 +80,7 @@ export default function UserSearch({ navigation }) {
             }}
         />
         <FlatList 
+            keyboardShouldPersistTaps='always'
             data={contentList}
             renderItem={renderItem}
             keyExtractor={item => item.id}
